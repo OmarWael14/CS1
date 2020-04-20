dbd611dd2e3571a86e4908c14dd1bc4e7912d119
lailaelsaeed <lailaelsaeed@aucegypt.edu>
2020-04-20 21:59:50 +0200

    message

---
+++ headers/laila.h
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+void print()
+{
+	cout<<"My username is lailaelsaeed"<<endl;
+	cout<<"My email is lailaelsaeed@aucegypt.edu"<<endl;
+}
---
+++ sources/lailaelsaeed.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include"laila.h"
+using namespace std;
+int main()
+{
+	print();
+}
