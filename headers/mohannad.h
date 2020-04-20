8978b3230547db1593762a9c2b101713190c2958
mohannadmonir <mohannadmonir@aucegypt.edu>
2020-04-20 21:56:43 +0200

    message

---
+++ headers/mohannad.h
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+void print()
+{
+	cout<<"My username is mohannadmonir"<<endl;
+	cout<<"My email is mohannadmonir@aucegypt.edu"<<endl;
+
+}
---
+++ sources/mohannad.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include"mohannad.h"
+
+using namespace std;
+int main()
+{
+print();
+}
